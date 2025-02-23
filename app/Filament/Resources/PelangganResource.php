e030ad48da2da16b3944f08b1e27663e327eee0d
Michael <michaeldiputra257@gmail.com>
2025-02-24 07:57:48 +0800

    first commit

---
+++ app/Filament/Resources/PelangganResource.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables;
+use Filament\Forms\Form;
+use App\Models\Pelanggan;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\PelangganResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\PelangganResource\RelationManagers;
+
+class PelangganResource extends Resource
+{
+    protected static ?string $model = Pelanggan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('nama_pelanggan')->required(),
+                TextInput::make('nomor_telepon')->required()->type('tel')->unique(ignoreRecord: true),
+                Textarea::make('alamat')->columnSpanFull()->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama_pelanggan')->sortable()->searchable(),
+                TextColumn::make('nomor_telepon')->searchable(),
+                TextColumn::make('alamat')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPelanggans::route('/'),
+            'create' => Pages\CreatePelanggan::route('/create'),
+            'edit' => Pages\EditPelanggan::route('/{record}/edit'),
+        ];
+    }
+}
